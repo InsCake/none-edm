08fca525869cc7b8798a2b9d3e5c428d88af534f
Cache Cake <m.saihomi@gmail.com>
2013-12-13 18:22:08 +0800

    0.0.2.1

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,30 +15,29 @@ noneEDMControllers.controller('newEDMCtrl', ['$scope',
 	function ($scope) {
 		//游标
 		var floorIndex = 0,
-			roomIndex = 0,
-			rowIndex = 0;
+			roomIndex = 0;
+		/*rowIndex = 0;*/
 
 		//设置 楼层、房间、行的初始状态
+		/*var _rows = [
+			{
+				id: rowIndex
+			}
+		];*/
 		var _rooms = [
 			{
 				id: roomIndex
 			}
 		];
-		var _rows = [
-			{
-				id: rowIndex
-			}
-		];
 		$scope.floors = [
 			{
 				id: 0,
-				rooms:_rooms,
-				rows:_rows
+				rooms: _rooms
 			}
 		];
 
 		$scope.roomQuantity = 1;
-		$scope.rowQuantity = 1;
+		/*$scope.rowQuantity = 1;*/
 
 
 		//新建一层
@@ -59,13 +58,13 @@ noneEDMControllers.controller('newEDMCtrl', ['$scope',
 			}
 		});
 		//监控行数设置
-		$scope.$watch('rowQuantity', function () {
+		/*$scope.$watch('rowQuantity', function () {
 			$scope.floors[floorIndex].rows = [];
 			for (var i = 0; i < $scope.rowQuantity; i++) {
 				$scope.floors[floorIndex].rows.push({
 					id: (i + 1)
 				});
 			}
-		});
+		});*/
 
   }]);
--- app/js/directives.js
+++ app/js/directives.js
@@ -1,11 +1,18 @@
 'use strict';
 
 /* Directives */
+var noneEDMDirectives = angular.module('noneEDM.directives', []);
 
-
-angular.module('noneEDM.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
+noneEDMDirectives.directive('appVersion', ['version', function (version) {
+		return function (scope, elm, attrs) {
+			elm.text(version);
+		};
   }]);
+
+noneEDMDirectives.directive('roomspace', function () {
+	return {
+		restrict: 'EA',
+		replace: true,
+		template: '<table><tr><td>' + '<img src="http://ww3.sinaimg.cn/large/93884909gw1ea0x8ev6rbj20640640sp.jpg">' + '</tr></td></table>'
+	};
+});
