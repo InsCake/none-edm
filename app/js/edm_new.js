c8e0b41663c807d0732e751e9d1b8aded1766408
Cache Cake <m.saihomi@gmail.com>
2014-01-10 18:11:59 +0800

    0.0.4

---
+++ app/js/edm_new.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    var action_bar = {
+        newFloor_showTP: function() {
+            $('.action_bar').on('click', '#newFloor_btn', function() {
+               	
+            });
+        }
+    };
+    action_bar.newFloor_showTP();
+});
